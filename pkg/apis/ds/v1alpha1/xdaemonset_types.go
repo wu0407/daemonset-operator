f6890506639ad72d604820683c5a6b54d40dd15a
wu0407 <xiaoqingnb@gmail.com>
2020-05-15 15:03:56 +0800

    修改spec

--- pkg/apis/ds/v1alpha1/xdaemonset_types.go
+++ pkg/apis/ds/v1alpha1/xdaemonset_types.go
@@ -2,6 +2,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	appsv1 "k8s.io/api/apps/v1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -12,6 +13,7 @@ type XdaemonsetSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	appsv1.DaemonSetSpec
 }
 
 // XdaemonsetStatus defines the observed state of Xdaemonset
