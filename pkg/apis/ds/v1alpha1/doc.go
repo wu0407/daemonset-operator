3ba595ca4a7102837cbba04a8b7e4404593f4928
wu0407 <xiaoqingnb@gmail.com>
2020-05-31 12:07:53 +0800

    添加defaulter-gen tag

--- pkg/apis/ds/v1alpha1/doc.go
+++ pkg/apis/ds/v1alpha1/doc.go
@@ -1,4 +1,5 @@
 // Package v1alpha1 contains API Schema definitions for the ds v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
+// +k8s:defaulter-gen=XdaemonsetSpec
 // +groupName=ds.xiaoqing.com
 package v1alpha1
